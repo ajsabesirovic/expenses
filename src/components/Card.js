66dec80cf9ac016c91c55b56e2ec3f4da18a6b65
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-01-04 19:05:12 +0100

    3

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 import './Card.css';
 
-function Card(){
+function Card(props){
     const classes = 'card ' + props.className;
     return (
-        <div className={classes}></div>
+        <div className={classes}>{props.children}</div>
     );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem"
 import './Expenses.css'
 
 function Expenses({expenses}){
     return(
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem
                 title={expenses[0].title}
                 amount={expenses[0].amount}
@@ -26,7 +27,7 @@ function Expenses({expenses}){
               ></ExpenseItem>
         
 
-        </div>)
+        </Card>)
 
 }
 
