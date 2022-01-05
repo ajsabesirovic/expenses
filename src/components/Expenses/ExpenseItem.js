cdf7b879c33781955f0927f8f5980ae496d35ae2
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-01-05 16:59:57 +0100

    5

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import Card from './Card';
+import React, { useState } from 'react';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem({title,amount,date}){
+    const [titl,setTitle] = useState(title);
+    const changeTitle = () => {
+        setTitle('updated')
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{titl}</h2>
                 <div className='expense-item__price'>${amount}</div>
             </div>
+            <button onClick={changeTitle}>Change the title</button>
         </Card>
     );
 };
---
+++ src/components/NewExpenses/NewExpenses.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './NewExpenses.css'
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(){
+    return(
+        <div className="new-expense">
+            <ExpenseForm>
+                
+            </ExpenseForm>
+        </div>
+    )
+}
+export default NewExpense;
